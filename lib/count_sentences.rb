5a6147a600bb83a799491815e050183affb3f28c
Andrew Williams <1williamsaj@gmail.com>
2019-09-23 00:08:22 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,14 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?(".")
-      return true
+    if self.end_with?(".") # Checks self to see if it ends with a periods
+      return true # If it does, return true value
     else
-      return false
+      return false # Otherwise, return false value
     end
   end
 
   def question?
+
+    if self.end_with?("?")
+      return true
+    end
 
   end
 
