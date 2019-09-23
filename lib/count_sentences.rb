23b0202eac68ecd78452889cbc2ec41dfa9956cb
Andrew Williams <1williamsaj@gmail.com>
2019-09-23 00:27:07 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -37,7 +37,7 @@ class String
 
     our_split = self.split
     our_split.each do |x|
-      if x.end_with?(".") || x.end_with?("!") || x.end_with("?")
+      if x[-1]
         symbols << x
       end
 
