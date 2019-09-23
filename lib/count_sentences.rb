2d8bb9938da24d36c5e5f6165487f97294d6b6b7
Andrew Williams <1williamsaj@gmail.com>
2019-09-23 00:36:39 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -34,9 +34,10 @@ class String
 
   def count_sentences
 
-    our_split = self.split
-    our_split.count {|x| x[-1] == "." || x[-1] == "!" || x[-1] == "?"}
-
+    our_split = self.split # First, we take out string and split each word to fill an array.
+    our_split.count {|x| x[-1] == "." || x[-1] == "!" || x[-1] == "?"} # Then we use count to iterate over each word to check the last character.
+    # 'x[-1]' returns the last character of the word in question. We check and see if that last character equals any of our punctuation that ends a sentence.
+    # If it matches, +1 is added to the count. A final tally is returned once the whole array has been checked.
 
   end
 
